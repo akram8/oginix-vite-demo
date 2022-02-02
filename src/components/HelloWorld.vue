92dc18bb498ab5ae2e539e09f2333bfd5dc238a4
akram8 <akram868@163.com>
2022-02-02 23:40:12 +0800

    test the element-plus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,28 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-
+import { ElMessage } from 'element-plus'
 defineProps<{ msg: string }>()
+
+const open = () => {
+  ElMessage({
+    message: 'this is a message.',
+    grouping: true,
+    type: 'success',
+  })
+}
 
 const count = ref(0)
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
+  <el-check-tag checked style="margin-right: 8px">Let's start the show!!!</el-check-tag>
   <p>
     Recommended IDE setup:
     <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
     +
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
   </p>
-
-  <p>See <code>README.md</code> for more information.</p>
 
   <p>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">
@@ -26,11 +32,9 @@ const count = ref(0)
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <el-button type="button" @click="count++">count is: {{ count }}</el-button>
+
+  <el-button :plain="true" @click="open">Show message</el-button>
 </template>
 
 <style scoped>
