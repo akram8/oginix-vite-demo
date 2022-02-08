91af70202f0ea885b5b276d59b920c6d921e8b57
akram8 <akram868@163.com>
2022-02-08 23:50:22 +0800

    test home page

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './theme/index.scss'
+// import * as svg from '@element-plus/icons-vue';
 
 const app = createApp(App)
 app.use(router)
