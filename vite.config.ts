dade70a5be9b9f8a6e80b816bcc90eeb558ad378
akram8 <akram868@163.com>
2023-03-30 23:45:56 +0800

    rewrite file name

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import vueSetupExtend from 'vite-plugin-vue-setup-extend-plus';
 import viteCompression from 'vite-plugin-compression';
 import { buildConfig } from './src/utils/build';
 
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 const pathResolve = (dir: string) => {
 	return resolve(__dirname, '.', dir);
 };
@@ -55,6 +59,16 @@ const viteConfig = defineConfig((mode: ConfigEnv) => {
 							return id.toString().split('node_modules/')[1].split('/')[0].toString();
 						}
 					},
+					sanitizeFileName(name) {
+						const match = DRIVE_LETTER_REGEX.exec(name);
+						const driveLetter = match ? match[0] : "";
+						// A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+						// Otherwise, avoid them because they can refer to NTFS alternate data streams.
+						return (
+							driveLetter +
+							name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+						);
+					}
 				},
 				external: JSON.parse(env.VITE_OPEN_CDN) ? buildConfig.external : [],
 			},
