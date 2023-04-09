71396e73b4dd698e2b9e6ed2bc6bc05bd9bb2da1
akram8 <akram868@163.com>
2023-04-09 23:59:53 +0800

    feat: ...

---
+++ src/i18n/pages/login/ug-cn.ts
@@ -0,0 +1,28 @@
+// 定义内容
+export default {
+	label: {
+		one1: 'ئابونىت نامى بىلەن كىرىش',
+		two2: 'تىلفۇن نۇمۇرى بىلەن كىرىش',
+	},
+	link: {
+		one3: 'ئۈچۈنچى تەرپ ئارقىلىق كىرىش',
+		two4: 'دوستانە ئۇلانما',
+	},
+	account: {
+		accountPlaceholder1: '用户名 admin 或不输均为 common',
+		accountPlaceholder2: '密码：123456',
+		accountPlaceholder3: 'دەلىلەش نۇمۇرىنى كىرگۈزۈڭ',
+		accountBtnText: 'كىرىش',
+	},
+	mobile: {
+		placeholder1: 'تېلفۇن نۇمۇرىنى كىرگۈزۈڭ',
+		placeholder2: 'دەلىلەش نۇمۇرىنى كىرگۈزۈڭ',
+		codeText: 'دەلىلەش نۇمۇرى',
+		btnText: 'كىرىش',
+		msgText: '* 温馨提示：建议使用谷歌、Microsoft Edge，版本 79.0.1072.62 及以上浏览器，360浏览器请使用极速模式',
+	},
+	scan: {
+		text: '打开手机扫一扫，快速登录/注册',
+	},
+	signInText: '!قايتىپ كەلگىنىڭىزنى قارشى ئالىمىز',
+};
