8a7e2c46de0928aa6cd5c67d9a2b8a6afdd39c07
akram8 <akram868@163.com>
2024-01-01 23:10:53 +0800

    feat: create chatSendPanel.vue file for chats

---
+++ src/views/chat/components/chatSendPanel.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- 消息发送面板 -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
